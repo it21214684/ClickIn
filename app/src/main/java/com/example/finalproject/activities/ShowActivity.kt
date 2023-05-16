a055a508e679d6e11612f177db3fddfee22a1686
IT21226182 <IT21226182@my.sliit.lk>
2023-05-16 11:14:32 +0530

    indentation 6

--- app/src/main/java/com/example/finalproject/activities/ShowActivity.kt
+++ app/src/main/java/com/example/finalproject/activities/ShowActivity.kt
@@ -6,6 +6,7 @@ import com.example.finalproject.R
 
 class ShowActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_show)//set the layout activity_show for ShowActivity
     }
