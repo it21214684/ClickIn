c1349cce1f6b286121d7ee9d22670ccdb4435ec8
IT21214448 <IT21214448@my.sliit.lk>
2023-05-16 11:12:17 +0530

    commit 20

--- app/src/main/java/com/example/madproject/activity/MainActivity.kt
+++ app/src/main/java/com/example/madproject/activity/MainActivity.kt
@@ -11,6 +11,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var button9: Button
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
