d9a3f211497504cf16ef89b49c716c975b4b8abd
IT21226182 <IT21226182@my.sliit.lk>
2023-05-16 11:13:28 +0530

    indentation 5

--- app/src/main/java/com/example/finalproject/activities/DashActivity.kt
+++ app/src/main/java/com/example/finalproject/activities/DashActivity.kt
@@ -6,7 +6,9 @@ import com.example.finalproject.R
 
 class DashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dash2)//set the layout activity_dash2 for DashActivity
+
     }
 }
