4c2ba1f4110ab39a8a2126442a992e8d849e1e86
IT21226182 <IT21226182@my.sliit.lk>
2023-05-16 11:15:38 +0530

    indentation 7

--- app/src/main/java/com/example/finalproject/activities/DelView.kt
+++ app/src/main/java/com/example/finalproject/activities/DelView.kt
@@ -6,7 +6,9 @@ import com.example.finalproject.R
 
 class DelView : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_del_view)//set the layout activity_del_view for DelView
+
     }
 }
